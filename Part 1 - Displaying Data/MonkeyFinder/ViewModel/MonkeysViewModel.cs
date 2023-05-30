f133a2282725a9e87747efa53d13cce3d14f66f1
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 16:18:31 +0200

    ViewModel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,41 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    private MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+    [RelayCommand]
+    private async Task GetMonkeyAsync()
+    {
+        if (IsBusy)
+            return;
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
