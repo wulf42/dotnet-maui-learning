f047076d69dc281775f5e04fbda7658ea09e85fe
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 16:18:16 +0200

    standard fields : isBusy, title
    used CommunityToolkit

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,17 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    private bool isBusy;
+
+    [ObservableProperty]
+    private string title;
+
+    public bool IsNotBusy => !IsBusy;
 }
