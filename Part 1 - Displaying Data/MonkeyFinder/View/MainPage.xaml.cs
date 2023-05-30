34860022178d1446b4f3383a777a862a1e0be035
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 16:15:45 +0200

    Binding Context

--- Part 1 - Displaying Data/MonkeyFinder/View/MainPage.xaml.cs
+++ Part 1 - Displaying Data/MonkeyFinder/View/MainPage.xaml.cs
@@ -2,9 +2,10 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage(MonkeysViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
 
