5f1b1078c248f126deae18bc5ff9eb3eb0a8452a
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 19:56:34 +0200

    registred services

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -19,6 +19,10 @@ public static class MauiProgram
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
+        builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+        builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+        builder.Services.AddSingleton<IMap>(Map.Default);
+
         builder.Services.AddSingleton<MonkeyService>();
         builder.Services.AddSingleton<MonkeysViewModel>();
         builder.Services.AddTransient<MonkeyDetailsViewModel>();
