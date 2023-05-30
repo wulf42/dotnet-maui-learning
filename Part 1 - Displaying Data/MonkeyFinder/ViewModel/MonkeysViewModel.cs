e6664bda99a98a25075eddf7c0298c7b5351f856
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 19:56:22 +0200

    Find closest by gelocation

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -6,11 +6,15 @@ public partial class MonkeysViewModel : BaseViewModel
 {
     private MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
+    private IConnectivity connectivity;
+    private IGeolocation geolocation;
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
 
     [RelayCommand]
@@ -34,6 +38,11 @@ public partial class MonkeysViewModel : BaseViewModel
             return;
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet issue", "No internet connection", "OK");
+                return;
+            }
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
 
@@ -51,6 +60,41 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+        }
+    }
+
+    [RelayCommand]
+    private async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+        try
+        {
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+            if (location is null)
+                return;
+            var first = Monkeys.OrderBy(m =>
+            location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Kilometers)).FirstOrDefault();
+
+            if (first is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest monkey",
+                $"{first.Name} in {first.Location}", "OK");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to get closest monkey: {ex.Message}", "OK");
         }
     }
 }
