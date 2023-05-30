fb97682899c9396b11b2b9b1271148c22705a289
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-30 20:05:16 +0200

    OpenMapAsync

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -3,10 +3,31 @@
 [QueryProperty(nameof(Monkey), "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel()
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map)
     {
+        this.map = map;
     }
 
     [ObservableProperty]
     public Monkey monkey;
+    [RelayCommand]
+    async Task OpenMapAsync()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Longitude, Monkey.Longitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            }); ;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", $"Unable to open map: {ex.Message}", "OK");
+        }
+
+    }
+
 }
